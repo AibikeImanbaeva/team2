1b85be39d45fbc5b7b226002d19d16d41e7b4530
Qwery <shamievmalik@gmail.com>
2023-01-31 18:27:23 +0600

    comment fixed

--- src/components/CommentCard/CommentList.jsx
+++ src/components/CommentCard/CommentList.jsx
@@ -1,21 +1,34 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { postContext } from '../../contexts/PostsContextProvider';
 import CommentCard from './CommentCard';
 import "./Comments.css"
 
-const CommentList = ({ onePost }) => {
-console.log(onePost)
+const CommentList = () => {
+  const { getPostDetails, onePost } = useContext(postContext);
+  const { id } = useParams();
+
+  useEffect(() => {
+    getPostDetails(id)
+  }, [])
+
   return (
-    <div className='comments'>
-      <div className='comments-title'>Комментарии</div>
-      <div className="comment-list">
-        <div className="comment-list__blur blur-top"></div>
-        {
-          onePost.commentaries?.map(comment =>
-            <CommentCard comment={comment} key={comment.id} />)
-        }
-        <div className="comment-list__blur blur-bottom"></div>
+    onePost ? (
+
+      <div className='comments'>
+        <div className='comments-title'>Комментарии</div>
+        <div className="comment-list">
+          <div className="comment-list__blur blur-top"></div>
+          {
+            onePost.commentaries?.map(comment =>
+              <CommentCard comment={comment} key={comment.id} />)
+          }
+          <div className="comment-list__blur blur-bottom"></div>
+        </div>
       </div>
-    </div>
+    ) : (
+            console.log("err")
+          )
   );
 };
 
